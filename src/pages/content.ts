8050e04c2b049aed3612938d29c5ebd8e9f600d3
runarissu <runarissu@gmail.com>
2025-03-18 12:25:32 +0700

    add page loading anim

--- src/pages/content.ts
+++ src/pages/content.ts
@@ -1,6 +1,28 @@
 import { OverlayScrollbars } from "overlayscrollbars";
+import { IEventAggregator, resolve } from "aurelia";
 
 export class Content {
+	readonly ea: IEventAggregator = resolve(IEventAggregator);
+	pageLoading: boolean = false;
+
+	constructor() {
+		this.ea.subscribe("au:router:navigation-end", (event) => {
+			this.pageLoading = false;
+		});
+
+		this.ea.subscribe("au:router:navigation-cancel", (event) => {
+			this.pageLoading = false;
+		});
+
+		this.ea.subscribe("au:router:navigation-error", (event) => {
+			this.pageLoading = false;
+		});
+
+		this.ea.subscribe("au:router:navigation-navigate", (event) => {
+			this.pageLoading = true;
+		});
+	}
+
 	attached() {
 		OverlayScrollbars(document.getElementById("content"), {
 			className: "os-theme-dark",
